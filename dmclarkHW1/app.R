613aaa197cc94c296c604f3eb3d6f7c43098e829
dmclark <104402036+V3ntingclark@users.noreply.github.com>
2023-02-01 17:30:05 -0500

    3rd commit: I cleaned up the grapics and added a third graph (hist) I need to work on the threshold 'button'

---
+++ dmclarkHW1/app.R
@@ -0,0 +1,93 @@
+# load the necessary libraries
+library(shiny)
+library(ggplot2)
+library(DT)
+
+#Importing my data
+
+US_Police_shootings_15_22 <- read.csv("~/dmclark/dmclark_hw1/US Police shootings in from 2015-22_alt.csv")
+
+# create a basic shiny app
+ui <- fluidPage(
+  
+  # add a title
+  titlePanel("Police Shooting Data from 2015 thur 2022"),
+  
+  # add a side panel for the inputs
+  sidebarLayout(
+    sidebarPanel(
+      
+      # create a drop-down menu for the variable
+      selectInput("variable", "Select a variable:", 
+                  choices = colnames(US_Police_shootings_15_22)),
+      
+      # create a numeric input for the threshold value
+      numericInput("threshold", "Enter threshold value:", 
+                   min = 0, max = 100, value = 50),
+      
+      # create a download button
+      downloadButton("downloadData", "Download Data")
+    ),
+    
+    # add a main panel for the outputs
+    mainPanel(
+      
+      # create a tabset for the plots and table
+      tabsetPanel(
+        
+        # create a tab for the line plot
+        tabPanel("Line Plot", plotOutput("linePlot")),
+        
+        # create a tab for the bar plot
+        tabPanel("Bar Plot", plotOutput("barPlot")),
+        
+        # create a tab for the histogram plot
+        tabPanel("Histogram", plotOutput("histogram")),
+        
+        # create a tab for the data table
+        tabPanel("Data Table", DT::dataTableOutput("dataTable"))
+        
+        
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  
+  output$linePlot <- renderPlot({
+    ggplot(US_Police_shootings_15_22, aes_string(x = "id" , y = input$variable)) +
+      geom_line()
+    
+  })
+  
+  # create the bar plot
+  output$barPlot <- renderPlot({
+    ggplot(US_Police_shootings_15_22, aes_string(x = input$variable, fill = "race")) +
+      geom_bar(position = "dodge")
+  })
+  
+  #create the Histogram
+  output$histogram <- renderPlot({
+    ggplot(US_Police_shootings_15_22, aes_string( x = 'date', y = input$variable ))
+  })
+  
+  # create the data table
+  output$dataTable <- DT::renderDataTable({
+    DT::datatable(subset(US_Police_shootings_15_22, US_Police_shootings_15_22$race > input$threshold))
+  })
+  
+  # create the download handler
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste("data-", Sys.Date(), ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(US_Police_shootings_15_22, file)
+    }
+  )
+}
+
+# run the shiny app
+shinyApp(ui = ui, server = server)
